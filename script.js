4ec367fac2b826996af9eb50ac98d912806f1734
ovitinho20 <vitor.poyer@escola.pr.gov.br>
2025-10-27 09:18:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const avanca = document.querySelectorAll('.btn-prximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+    })
+})
